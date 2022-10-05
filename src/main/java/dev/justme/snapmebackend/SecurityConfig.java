ed1ade122262f9470e58ac1e2b80c2a0b56022d1
ilja <ilja.ragnar@gmail.com>
2022-10-05 20:50:12 +0200

    secured everything except / and /error routes with oauth2

--- src/main/java/dev/justme/snapmebackend/SecurityConfig.java
+++ src/main/java/dev/justme/snapmebackend/SecurityConfig.java
@@ -4,22 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // http.authorizeRequests(a -> a.antMatchers("/", "/error").permitAll().anyRequest().authenticated()).exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))).oauth2Login();
-
         http.authorizeRequests(a -> {
             a.antMatchers("/", "/error").permitAll().anyRequest().authenticated();
         }).exceptionHandling(e -> {
             e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
-        }).oauth2Login();
+        }).oauth2Login().redirectionEndpoint(e -> {
+            e.baseUri("/");
+        });
         return http.build();
     }
 }
