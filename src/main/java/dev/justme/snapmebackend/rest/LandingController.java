06b6568c43e73909936077b71ddce827a07bc537
ilja <ilja.ragnar@gmail.com>
2022-10-05 20:49:41 +0200

    Made landing controller

---
+++ src/main/java/dev/justme/snapmebackend/rest/LandingController.java
@@ -0,0 +1,16 @@
+package dev.justme.snapmebackend.rest;
+
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LandingController {
+    @GetMapping("/")
+    public String landing() {
+        return "ACK";
+    }
+}
